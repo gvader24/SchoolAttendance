54e0d36393d915557f411003ade6d4e7e188feaf
gvader24 <44652796+gvader24@users.noreply.github.com>
2018-10-31 15:07:36 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+# Single Line Comment
+'''
+Multi Line comment
+
+Need to download Anaconda
+'''
+
+# Python is dynamically typed
+
+# int x = 5;
+
+x = "Adarsh"
+y = 'a'
+z = 5
+
+List = []
+List.append(5)
+List.append(6)
+List.append("Adarsh")
+List.append(["A",2,3.14,'c'])
+
+for eachElement in List:
+  print(eachElement)
+
+print(List)
